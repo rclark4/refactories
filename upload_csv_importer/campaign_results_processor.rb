e0da624165e95fb8e0c54fce6aff1cb7b926694a
Frank Fuston <Frank.fuston@gmail.com>
2014-07-01 19:46:47 -0500

    Merge in other CSV functionality

--- upload_csv_importer/campaign_results_processor.rb
+++ upload_csv_importer/campaign_results_processor.rb
@@ -19,17 +19,30 @@ class CSVThing
     file.content_type == "text/csv"
   end
 
-  def acceptable_format?
-    options = { :headers => true, :col_sep => ",", :quote_char => "\x00" }
-    headers = FasterCSV.read(file.path, options).headers.compact
+  def size
+    csv.size
+  end
 
-    acceptable_formats.any? { |format| format == headers }
+  def stage path
+    File.open(path, "w") { |f| f.write(file.read) }
+  end
+
+  private
+
+  def csv
+    options = { :headers => true, :col_sep => ",", :quote_char => "\x00" }
+    FasterCSV.read(file.path, options)
+  end
+
+  def acceptable_format?
+    acceptable_formats.any? { |format| format == csv.headers.compact }
   end
 
   def acceptable_formats
     [
       ["Email", "Date", "Activity", "Location", "\"Email Client\"", "URL", "Groups"],
-      ["Title", "URL", "\"Unique Clicks\"", "\"Total Clicks\"", "\"Most Recent\"", "Location"]
+      ["Title", "URL", "\"Unique Clicks\"", "\"Total Clicks\"", "\"Most Recent\"", "Location"],
+      ["first_name", "last_name", "email", "company", "title", "street", "street2", "city", "state", "zip_code", "phone", "phone2", "home_phone", "mobile_phone", "fax", "home_fax"]
     ]
   end
 end
