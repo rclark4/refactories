83f289fdfd3948d2f2d80fe8c168dcb9f4aae9de
Frank Fuston <Frank.fuston@gmail.com>
2014-07-01 19:27:03 -0500

    move headers and formatting methods to CSVThing class

--- upload_csv_importer/campaign_results_processor.rb
+++ upload_csv_importer/campaign_results_processor.rb
@@ -11,27 +11,6 @@ class CampaignResultsProcessor
   def formatted?
     activities_csv? && links_csv? && activities_formatted_headers? && links_formatted_headers?
   end
-
-  def activities_formatted_headers?
-    headers(activities) == activities_formatted_headers
-  end
-
-  def links_formatted_headers?
-    headers(links) == links_formatted_headers
-  end
-
-  def headers(file)
-    options = { :headers => true, :col_sep => ",", :quote_char => "\x00" }
-    FasterCSV.read(file.path, options).headers.compact
-  end
-
-  def activities_formatted_headers
-    ["Email", "Date", "Activity", "Location", "\"Email Client\"", "URL", "Groups"]
-  end
-
-  def links_formatted_headers
-    ["Title", "URL", "\"Unique Clicks\"", "\"Total Clicks\"", "\"Most Recent\"", "Location"]
-  end
 end
 
 class CSVThing
@@ -43,5 +22,15 @@ class CSVThing
 
   def csv?
     file.content_type == "text/csv"
+  end
+
+  def headers
+    options = { :headers => true, :col_sep => ",", :quote_char => "\x00" }
+    FasterCSV.read(file.path, options).headers.compact
+  end
+
+  def formatted_headers
+    headers == ["Email", "Date", "Activity", "Location", "\"Email Client\"", "URL", "Groups"] ||
+    headers == ["Title", "URL", "\"Unique Clicks\"", "\"Total Clicks\"", "\"Most Recent\"", "Location"]
   end
 end
